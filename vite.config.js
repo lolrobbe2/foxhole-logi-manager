cc254dbfcf24e8c867a889d33f3ad81c20c5e785
Robbe Beernaert <robbebeernaert@hotmail.be>
2025-08-12 12:35:46 +0200

    moved folder

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/foxhole-logi-manager/',  // <-- your repo name here
-  plugins: [react()],
-})
