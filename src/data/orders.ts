fddd0b901e2dfd918c77766faf760ac595ec1f41
Robbe Beernaert <robbebeernaert@hotmail.be>
2025-08-25 13:50:18 +0200

    updated logisheet uri

--- src/data/orders.ts
+++ src/data/orders.ts
@@ -64,7 +64,6 @@ export class OrderManager {
 		if (existing) {
 			throw new Error(`Order "${order.name}" already exists.`)
 		}
-		console.log(order);
 		const newOrder: Order = {
 			...order,
 			items: order.items ?? [],
